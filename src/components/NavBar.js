a4b716a2bfc7cf98530cfd8e008d8fe8b60bfc18
xxemmaxx <emmanuelpedro2111@gmail.com>
2021-07-27 00:05:54 -0300

    version 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,22 @@ import React from 'react';
  function NavBar() {
     return (
       <section className="container-fluid">
-<Nav variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>
-  </section>
+<h1 className="text-center">Tienda Kairos</h1>
+
+
+     <nav>
+  <div className="nav nav-tabs" id="nav-tab" role="tablist">
+    <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+    <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Product</button>
+    <button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+  </div>
+</nav>
+<div className="tab-content" id="nav-tabContent">
+  <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">a</div>
+  <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">b</div>
+  <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">c</div>
+</div>
+      </section>
     );
   }
   export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import Button from 'react-bootstrap/Button';
 import NavBar from './components/NavBar'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 // ReactDOM.render(<App />, document.getElementById('root'));
